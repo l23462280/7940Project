977f6d7d71943cd3345088b515daae83d75fb7a0
x x <23462280@life.hkbu.edu.hk>
2024-04-01 00:19:42 +0800

    3.31 deploy

--- chatbot.py
+++ chatbot.py
@@ -216,6 +216,10 @@ def db_connection():
 
 def main():
     telegram_access_token = os.getenv('TELEGRAM_ACCESS_TOKEN')
+    if 'TELEGRAM_ACCESS_TOKEN' in os.environ:
+        print('TELEGRAM_ACCESS_TOKEN is set')
+    else:
+        print('TELEGRAM_ACCESS_TOKEN is not set')
     application = Application.builder().token(telegram_access_token).build()
 
     start_handler = CommandHandler('start', start)
