dfd13bd8f0f95c80b2df729a1fc3d82ad4a6cbc4
x x <23462280@life.hkbu.edu.hk>
2024-04-01 00:05:05 +0800

    3.31 deploy

--- chatbot.py
+++ chatbot.py
@@ -215,7 +215,7 @@ def db_connection():
 
 
 def main():
-    telegram_access_token = os.environ['TELEGRAM_ACCESS_TOKEN']
+    telegram_access_token = os.getenv('TELEGRAM_ACCESS_TOKEN')
     application = Application.builder().token(telegram_access_token).build()
 
     start_handler = CommandHandler('start', start)
